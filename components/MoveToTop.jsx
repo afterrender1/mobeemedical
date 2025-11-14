06d1dde74f059f75efb2c2238a4e5865d70420ea
afterrender1 <afterrender1@gmail.com>
2025-11-14 17:55:03 +0500

    Update MoveToTop.jsx

--- components/MoveToTop.jsx
+++ components/MoveToTop.jsx
@@ -7,7 +7,7 @@ const MoveToTop = () => {
   return (
       <button
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        className="fixed bottom-8 right-8 w-14 h-14 bg-[#066BAA] text-white rounded-full flex items-center justify-center shadow-xl hover:bg-[#055996] transition z-40"
+        className="fixed bottom-8 right-8 w-14 cursor-pointer h-14 bg-[#066BAA] text-white rounded-full flex items-center justify-center shadow-xl hover:bg-[#055996] transition z-40"
         aria-label="Back to top"
       >
         <ArrowUp className="w-6 h-6" />
